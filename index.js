e6499f0fe799e7cbe9e8071a133ecdc455f2833d
Leon Kinyanjui Maingi <leon.kinyanjui@student.moringaschool.com>
2023-09-18 00:30:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const namesArray = ["Alice", "Bob", "Charlie"];
+const event = "birthday";
+const thankYouMessages = writeCards(namesArray, event);
+
+function writeCards(names, eventName) {
+    const messages = [];
+}
+for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+console,log(thankYouMessages);
